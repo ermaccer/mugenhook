bfc13661b32f74f47cb818120e6c4856780f80ec
ermaccer <40604575+ermaccer@users.noreply.github.com>
2019-09-20 14:19:31 +0200

    Add files via upload

--- source/dllmain.cpp
+++ source/dllmain.cpp
@@ -7,7 +7,11 @@ extern "C"
 {
 	__declspec(dllexport) void InitializeASI()
 	{
-		Init();
+		if (*(char*)0x40B97B == 0 && *(char*)0x40B97D == 1 && *(char*)0x40B97F == 1)
+			Init();
+		else
+			MessageBoxA(0, "Invalid M.U.G.E.N version! Only 1.1.0 supported!", "MugenHook", MB_ICONERROR);
+
 	}
 }
 
